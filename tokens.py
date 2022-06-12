e87c6bc1b28eb56a8b59b312e793eae39297477d
redone <elkaboussi@pm.me>
2022-06-12 13:19:28 +0100

    setup a token file

---
+++ tokens.py
@@ -0,0 +1,8 @@
+# the twitterAPI rate limit is probably not enough, in this case you may consider
+# creating multiple tokens from multiple accounts
+
+tokens = [
+    "first_token",
+    "second_token",
+    "..."
+]
