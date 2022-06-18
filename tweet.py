c85890d46108e646754ae26446d035d51a099458
RedOne <elkaboussi@pm.me>
2022-06-18 21:31:55 +0100

    add tweet from cmd

---
+++ tweet.py
@@ -0,0 +1,26 @@
+from helium import *
+from . import creds
+
+username = creds.username
+password = creds.password
+
+tweet = " " + input("Tweet? ")
+
+
+try:
+    start_chrome("https://twitter.com/login", headless=True)
+    wait_until(Text("Sign").exists)
+    write(username, into="Phone, email, or username")
+    click("Next")
+    wait_until(Text("Enter your password").exists)
+    write(password, into="Password")
+    click("Log in")
+    wait_until(Text("Home").exists)
+    write(tweet, into="What’s happening?")
+    click("Tweet")
+
+except:
+    print("Something went wrong")
+
+else:
+    print("Tweeted Successfully")
